44c4c6d85d168de18a7e38774e3b7d6755efbea5
Uli <uaqro@protonmail.com>
2021-02-16 11:15:33 -0600

    url

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,74 +4,13 @@ import { Route } from "react-router-dom";
 function Home() {
   const randomizer = Math.floor(Math.random() * 6);
   return (
-    <div>
-      {(() => {
-        switch (randomizer) {
-          case 1:
-            return (
               <Route
                 path="/"
                 component={() => {
-                  window.location.href = "https://forms.gle/1szPPqG5SbeArzyMA";
+                  window.location.href = "https://www.eternatulum.mx";
                   return null;
                 }}
               />
-            );
-          case 2:
-            return (
-              <Route
-                path="/"
-                component={() => {
-                  window.location.href = "https://forms.gle/4JvnkVrm5zD9k4BY7";
-                  return null;
-                }}
-              />
-            );
-          case 3:
-            return (
-              <Route
-                path="/"
-                component={() => {
-                  window.location.href = "https://forms.gle/HKWuE8MzXhDcQywGA";
-                  return null;
-                }}
-              />
-            );
-          case 4:
-            return (
-              <Route
-                path="/"
-                component={() => {
-                  window.location.href = "https://forms.gle/A48hZtN27zWXm9DKA";
-                  return null;
-                }}
-              />
-            );
-          case 5:
-            return (
-              <Route
-                path="/"
-                component={() => {
-                  window.location.href = "https://forms.gle/4dgBNj6FCnK3E7j57";
-                  return null;
-                }}
-              />
-            );
-          case 6:
-            return (
-              <Route
-                path="/"
-                component={() => {
-                  window.location.href = "https://forms.gle/ZzNRi6buVQENgvdb7";
-                  return null;
-                }}
-              />
-            );
-          default:
-            return <h1>Error</h1>;
-        }
-      })()}
-    </div>
   );
 }
 
